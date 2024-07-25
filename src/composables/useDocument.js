e44e7281475fe75549d358b619d1ec6bf9e3aad9
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-07-25 20:27:03 +0200

    Resolved conflict in useDocument.js and applied stashed changes

--- src/composables/useDocument.js
+++ src/composables/useDocument.js
@@ -1,36 +1,38 @@
 import { ref } from 'vue'
 import { projectFirestore } from '@/firebase/config'
-import getUser from '@/composables/getUser';  // Import getUser composable to get user information
+import getUser from '@/composables/getUser'
 
 const useDocument = (collection, id, userId = null) => {
   const error = ref(null)
   const isPending = ref(false)
-  const { user } = getUser();  // Get user information from the composable
+  const { user } = getUser()
 
   let documentRef = projectFirestore.collection(collection).doc(id)
 
-  // If userId is not provided, use the userId from the user object
   if (!userId && user.value) {
-    userId = user.value.uid;
+    userId = user.value.uid
   }
 
   const updateDoc = async (updates) => {
-    isPending.value = true;
-    error.value = null;
-  
-    try {
-      if ('userId' in updates && updates.userId !== userId) {
-        throw new Error('Unauthorized update attempt');
-      } 
-      const res = await documentRef.update(updates)
-      isPending.value = false
-      return res
-    } catch (err) {
-      isPending.value = false
-      error.value = 'could not update document'
-    }
+    isPending.value = true
+    error.value = null
 
-  };
+    try {
+      console.log('Attempting to update document with:', updates)
+      if ('userId' in updates && updates.userId !== userId) {
+        throw new Error('Unauthorized update attempt')
+      }
+      await documentRef.update(updates)
+      console.log('Document updated successfully')
+      isPending.value = false
+      return { success: true, message: 'Update successful' }
+    } catch (err) {
+      console.error('Error updating document:', err.message)
+      isPending.value = false
+      error.value = `Could not update document: ${err.message}`
+      return { success: false, message: err.message }
+    }
+  }
 
   return { error, isPending, updateDoc }
 }
