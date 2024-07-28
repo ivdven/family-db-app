79488505e159d32a1b6385478e7d722ebafda18e
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-07-28 19:46:14 +0200

    Added a route guard to prevent user from changing other user's data

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,39 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '@/views/auth/Login.vue'
 import Signup from '@/views/auth/Signup.vue'
+import DetailView from '@/views/DetailView'
+
+import { projectAuth } from '@/firebase/config'
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = projectAuth.onAuthStateChanged(user => {
+      removeListener()
+      resolve(user)
+    }, reject)
+  })
+}
+
+const requireAuth = async (to, from, next) => {
+ try {
+  const user = await getCurrentUser()
+  if (user) {
+    next()
+  } else {
+    next({ name: 'Login' })
+  }
+ } catch (err) {
+   console.log('Error checking authentication status: ', err)
+   next({ name: 'Login' })
+ }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: HomeView,
+    beforeEnter: requireAuth
   },
   {
     path: '/login',
@@ -19,6 +46,13 @@ const routes = [
     name: 'Signup',
     component: Signup
   },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    component: DetailView,
+    beforeEnter: requireAuth,
+    props: true
+  }
 ]
 
 const router = createRouter({
