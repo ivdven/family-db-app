143dec8c7337c0ef6f65058aa8c5e3d6af39ec25
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-08-22 19:50:11 +0200

    Added a view to display all user's data

---
+++ src/views/FamilyMembersView.vue
@@ -0,0 +1,32 @@
+<template>
+ <div v-if="documents">
+  <ListView 
+    :documents="documents" />
+ </div>
+ <div class="error" v-else>
+  {{ error }}
+ </div>
+</template>
+
+<script>
+import ListView from '@/components/ListView.vue'
+
+import getCollection from '@/composables/getCollection'
+
+export default {
+  components: {
+    ListView
+  },
+  setup(){
+     const { error, documents } = getCollection('familyMembers')
+    
+    return { 
+      error, documents
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
