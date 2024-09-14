19a8f22af10995dac07711959d82972c6d4d2826
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-09-14 16:42:44 +0200

    Added a computed property called "filteredDocuments" and pass them as prop to our child component ListView.

--- src/views/FamilyMembersView.vue
+++ src/views/FamilyMembersView.vue
@@ -1,7 +1,14 @@
 <template>
  <div v-if="documents">
-  <ListView 
-    :documents="documents" />
+  <div class="search-bar">
+    <SearchBar @search="handleSearch" />
+  </div>
+  <div>
+    <ListView
+      :documents="filteredDocuments"
+      :isSearching="isSearching"
+    />
+  </div>
  </div>
  <div class="error" v-else>
   {{ error }}
@@ -10,23 +17,50 @@
 
 <script>
 import ListView from '@/components/ListView.vue'
+import SearchBar from '@/components/SearchBar.vue'
 
 import getCollection from '@/composables/getCollection'
 
+import { computed, ref } from 'vue'
+
 export default {
   components: {
-    ListView
+    ListView,
+    SearchBar
   },
   setup(){
-     const { error, documents } = getCollection('familyMembers')
+    const { error, documents } = getCollection('familyMembers')
+
+    const searchQuery = ref('')
+    const isSearching = ref(false)
+
+    const handleSearch = (query) => {
+      searchQuery.value = query
+    }
+
+    const filteredDocuments = computed(() => {
+      const query = searchQuery.value.toLowerCase()
+      if (query) {
+        isSearching.value = true
+        return documents.value.filter((document) => {
+          return document.familyName.toLowerCase().includes(query)
+        })
+      } else {
+        isSearching.value = false
+        return documents.value
+      }
+    })
     
     return { 
-      error, documents
+      error, documents, searchQuery, isSearching, handleSearch, filteredDocuments
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .search-bar {
+    display: flex;
+    justify-content: flex-end;
+  }
 </style>
