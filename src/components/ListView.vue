25d43925e7a8fb2afcca26347f2df82988f14796
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-09-14 16:46:21 +0200

    Implemented the filteredDocuments property from parent component and created another computed property called "displayedDocuments" for conditional rendering.

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,36 +1,47 @@
 <template>
 	<div class="members-card">
 	  <div class="card">
-			<div class="document" v-for="document in sortedDocuments" :key="document.id">
-				<p class="styled-text">Family {{ document.familyName }}</p>
-				<router-link class="detail-link btn" :to="{ name: 'Detail', params: { id: document.id } }">See data</router-link>
-			</div>
+				<div class="document" v-for="document in displayedDocuments" :key="document.id">
+					<p class="styled-text">Family {{ document.familyName }}</p>
+					<router-link class="detail-link btn" :to="{ name: 'Detail', params: { id: document.id } }">
+						See data
+					</router-link>
+				</div>
 		</div>
 	</div>
 </template>
 
 <script>
 
-import { computed, ref } from 'vue'
+import { computed, onMounted } from 'vue'
 
 export default {
 	props: {
-		documents: Object
+		documents: {
+			type: Array,
+			required: true
+		},
+		isSearching: {
+			type: Boolean,
+			required: true
+		}
 	},
 	setup(props) {
-
-		const userData = ref(props.documents)
-
+		
 		// sort the documents in alphabetic order by name
 		const sortedDocuments = computed(() => {
-			return userData.value.slice().sort((a, b) => {
+			return props.documents.slice().sort((a, b) => {
 				if (a.familyName < b.familyName) return -1
 				if (a.familyName > b.familyName) return 1
 				return 0
 			})
 		})
 
-		return { userData, sortedDocuments }
+		const displayedDocuments = computed(() => {
+			return props.isSearching ? props.documents : sortedDocuments.value
+		})
+
+		return { sortedDocuments, displayedDocuments }
 	}
 }
 </script>
