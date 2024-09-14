fc120812e908cd54059d943d3baee88b0d7f94bf
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-09-14 16:40:20 +0200

    Added a search bar component that we can apply on every other component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="search-bar">
+    <input type="text"
+      v-model="query"
+      @input="handleInput"
+      placeholder="Search family member">
+  </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+export default {
+  setup(_, { emit }) {
+
+    const query = ref('')
+    
+    const handleInput = () => {
+      emit('search', query.value)
+    }
+
+    return { query, handleInput }
+  }
+}
+</script>
+
+<style scoped>
+.search-bar {
+  display: flex;
+  justify-content: flex-end;
+}
+.search-bar input {
+  border: 1px solid var(--secondary);
+  border-radius: 8px;
+}
+</style>
