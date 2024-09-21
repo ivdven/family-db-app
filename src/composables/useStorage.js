11481086ff5963b0297e89abbc56224a3eeb8522
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-09-21 12:47:40 +0200

    Added a useStorage composable to upload images or other media content to project

---
+++ src/composables/useStorage.js
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+import { projectStorage } from '@/firebase/config'
+import getUser from './getUser'
+
+const useStorage = () => {
+
+  const { user } = getUser()
+
+  const error = ref(null)
+  const url = ref(null)
+  const filePath = ref(null)
+
+  const uploadImage = async (file, path) => {
+    filePath.value = `${path}/${user.value.uid}/${file.name}`
+    const storageRef = projectStorage.ref(filePath.value)
+
+    try {
+      const res = await storageRef.put(file)
+      url.value = res.ref.getDownloadURL()
+    } catch (err) {
+      console.log(err.value)
+      error.value = err.message
+    }
+  }
+
+  return { user, error, url, filePath, uploadImage }
+}
+
+export default useStorage
