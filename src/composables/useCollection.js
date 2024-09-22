027cc141424b9304a4ad5c6e89d0836c7792ef0f
Immanuel van de Ven <immanuelvandeven@gmail.com>
2024-09-22 16:01:01 +0200

    Added useCollection composable to add a document to a collection

---
+++ src/composables/useCollection.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue'
+import { projectFirestore } from '@/firebase/config'
+
+const useCollection = (collection) => {
+
+  const error = ref (null)
+  const isPending = ref(null)
+
+  const addDocument = async (doc) => {
+    error.value = null
+    isPending.value = true
+
+    try {
+      const res = await projectFirestore.collection(collection).add(doc)
+      isPending.value = false
+      return res
+    } catch (err) {
+      console.log(err.message)
+      error.value = err.message
+      isPending.value = false
+    }
+  }
+
+  return { error, isPending, addDocument }
+  
+}
+
+export default useCollection
